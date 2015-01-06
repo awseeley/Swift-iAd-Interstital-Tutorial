d05fd11accd692fc75c6cb278df9f678e075f1c2
Andrew Seeley <awseeley@gmail.com>
2015-01-06 18:04:59 +1100

    Swift iAd Interstital Tutorial
    Swift iAd Interstital Tutorial

--- iAdInterstital/ViewController.swift
+++ iAdInterstital/ViewController.swift
@@ -7,14 +7,74 @@
 //
 
 import UIKit
+import iAd
 
-class ViewController: UIViewController {
+
+class ViewController: UIViewController, ADInterstitialAdDelegate {
+    
+    var interAd = ADInterstitialAd()
+    var interAdView: UIView = UIView()
+    
+    var closeButton = UIButton.buttonWithType(UIButtonType.System) as UIButton
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        closeButton.frame = CGRectMake(10, 10, 20, 20)
+        closeButton.layer.cornerRadius = 10
+        closeButton.setTitle("x", forState: .Normal)
+        closeButton.setTitleColor(UIColor.blackColor(), forState: .Normal)
+        closeButton.backgroundColor = UIColor.whiteColor()
+        closeButton.layer.borderColor = UIColor.blackColor().CGColor
+        closeButton.layer.borderWidth = 1
+        closeButton.addTarget(self, action: "close:", forControlEvents: UIControlEvents.TouchDown)
+        
     }
 
+    func close(sender: UIButton) {
+        closeButton.removeFromSuperview()
+        interAdView.removeFromSuperview()
+    }
+    
+    @IBAction func showAd(sender: UIButton) {
+        loadAd()
+    }
+    
+    func loadAd() {
+        println("load ad")
+        interAd = ADInterstitialAd()
+        interAd.delegate = self
+    }
+    
+    func interstitialAdDidLoad(interstitialAd: ADInterstitialAd!) {
+        println("ad did load")
+        
+        interAdView = UIView()
+        interAdView.frame = self.view.bounds
+        view.addSubview(interAdView)
+        
+        interAd.presentInView(interAdView)
+        UIViewController.prepareInterstitialAds()
+        
+        interAdView.addSubview(closeButton)
+    }
+    
+    func interstitialAdDidUnload(interstitialAd: ADInterstitialAd!) {
+        
+    }
+    
+    func interstitialAd(interstitialAd: ADInterstitialAd!, didFailWithError error: NSError!) {
+        println("failed to receive")
+        println(error.localizedDescription)
+        
+        closeButton.removeFromSuperview()
+        interAdView.removeFromSuperview()
+        
+    }
+    
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
